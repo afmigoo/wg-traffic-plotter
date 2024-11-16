59cda533c22cad8e711dafcef1677397f64aa930
kesha <kesha@nuxt>
2024-11-16 22:55:51 +0100

    dump script improved

--- dump_log.sh
+++ dump_log.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-log_file='/home/kesha/wg/logs/traffic.log'
+root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
+python="$root/venv/bin/python3"
+script="$root/parse.py"
+log_file="$root/traffic.log"
+wgconf_file='/etc/wireguard/wg0.conf'
+output_file="/hosted/wgstat/plots.html"
+
 timestamp="[$(date '+%Y-%m-%d %H:%M:%S')]"
 
 echo "$timestamp Transfer bytes" >> $log_file
 wg show wg0 transfer | sed 's/\t/,/g' | sed "s/^/$timestamp /g" >> $log_file
+
+$python $script $wgconf_file $log_file $output_file
 

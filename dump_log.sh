2cca143f21641879817bc9cdddba88e9d625c36a
Elen Kartina <affenmilchmann@gmail.com>
2025-03-06 18:58:53 +0300

    script fix

--- dump_log.sh
+++ dump_log.sh
@@ -5,12 +5,12 @@ python="$root/venv/bin/python3"
 script="$root/render.py"
 log_file="$root/traffic.log"
 wgconf_file='/etc/wireguard/wg0.conf'
-output_file="/hosted/wgstat/plots.html"
+output_dir="/hosted/wgstat"
 
 timestamp="[$(date '+%Y-%m-%d %H:%M:%S')]"
 
 echo "$timestamp Transfer bytes" >> $log_file
 wg show wg0 transfer | sed 's/\t/,/g' | sed "s/^/$timestamp /g" >> $log_file
 
-$python $script $wgconf_file $log_file $output_file
+$python $script $wgconf_file $log_file $output_dir
 
